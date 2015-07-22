62e07e5b5bf61fc303da8ab8e29fbb365e46c33b
Justin Bureau <justin.bureau@gmail.com>
2015-07-21 23:49:44 -0300

    Scaffold + migration + migration w/ rating & finished_on

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Book.destroy_all
+
+Book.create!([{
+title: "Hypersion",
+author: "Dan Simmons",
+description: "Sci Fi.",
+amazon_id: "0553283685",
+rating: 5,
+finished_on: 10.day.ago
+},
+{
+title: "Jony Ive: The Genius Behind Apple's Greatest Products", 
+author: "Leander Kahney",
+description: "Ive master pieces explained.",
+amazon_id: "159184617X",
+rating: 3,
+finished_on: nil
+}])
+
+p "Created #{Book.count} books ;)"
